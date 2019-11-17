32b433476a2971bc168a84c433d38115e09afa2f
MaxDudko <max2410zp@gmail.com>
2019-11-17 18:07:43 +0200

    client-side/server-side folders, main dependencies

--- client/src/core/App.tsx
+++ client/src/core/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from '../logo.png';
 import style from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className={style.AppHeader}>
-        <h1>Assistant</h1>
-        <img src={logo} className={style.AppLogo} alt="logo" />
-        <p>
-          Virtual Assistant with many useful functions...
-        </p>
-        <a
-          className={style.AppLink}
-          href="https://github.com/MaxDudko/Assistant"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Work in Progress...
-        </a>
-      </header>
-    </div>
+    <iframe src="https://maxdudko.github.io/Assistant/"
+            className={style.App}
+            title="Assistant"
+            frameBorder="0"
+    />
   );
 };
 
