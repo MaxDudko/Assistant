42c72836a4bc32da626e844350793f3867045928
MaxDudko <max2410zp@gmail.com>
2019-11-21 18:17:15 +0200

    core/components

--- client/src/core/App.tsx
+++ client/src/core/App.tsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import style from './App.module.scss';
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import DashBoard from "./components/DashBoard/DashBoard";
+import Footer from "./components/Footer/Footer";
 
-interface IState {}
+interface IState {
+    isLogin: boolean,
+    userName: string,
+    isCollapsed: boolean,
+    SidebarItems: any,
+}
 
 class App extends React.Component<{}, IState> {
-  state: IState = {};
+  state: IState = {
+      isLogin: true,
+      userName: "Admin",
+      isCollapsed: false,
+      SidebarItems: [
+          {
+              name: "DashBoard",
+              icon: "faCheck"
+          },
+          {
+              name: "Contacts",
+              icon: "faPlus"
+          },
+          {
+              name: "Task List",
+              icon: "faCheck"
+          },
+          {
+              name: "Calendar",
+              icon: "faPlus"
+          },
+          {
+              name: "Notifications",
+              icon: "faCheck"
+          }
+      ],
+  };
 
+  collapsed() {
+      let collapsed = !this.state.isCollapsed;
+      this.setState({isCollapsed: collapsed})
+  }
   render() {
     return(
         <div className={style.App}>
-            <iframe src="https://maxdudko.github.io/Assistant/"
-                    className={style.App}
-                    title="Assistant"
-                    frameBorder="0"
-                    style={{
-                        width: "100%",
-                        height: "99.5vh",
-                        margin: "0",
-                        padding: "0",
-                    }}
+            <Navbar isLogin={this.state.isLogin}
+                    userName={this.state.userName}
             />
+            <div className={style.wrapper}>
+                <Sidebar isCollapsed={this.state.isCollapsed}
+                         collapsed={this.collapsed.bind(this)}
+                         SidebarItems={this.state.SidebarItems}
+                />
+                <DashBoard/>
+            </div>
         </div>
     )
   }
--- client/src/core/components/Navbar/Navbar.tsx
+++ client/src/core/components/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import style from "./Navbar.module.scss";
+import avatar from  "../../../assets/img/mern.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faCogs, faBell } from "@fortawesome/free-solid-svg-icons";
 
-interface IProps {}
+interface IProps {
+    isLogin: boolean,
+    userName: string,
+}
 
 const Navbar: React.FC<IProps> = (props) => {
     return (
         <div className={style.Navbar}>
-            Navbar
+            <FontAwesomeIcon icon={faBell}
+                             className={style.item}
+                             title="Notifications"
+            />
+            <FontAwesomeIcon icon={faCogs}
+                             className={style.item}
+                             title="Settings"
+            />
+            <FontAwesomeIcon icon={faSignOutAlt}
+                             className={style.item}
+                             title="SignOut"
+            />
         </div>
     )
 };
